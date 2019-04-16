40df5c8f92c9abaebef3199c34e74d0ea3f09a06
JustIceQAQ <justiceqaq@gmail.com>
2019-04-17 01:36:45 +0800

    修改輸出函式；增加輸出資料格式；修改說明

--- CTData.py
+++ CTData.py
@@ -5,54 +5,126 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import os
 import io
+import sys
+import tkinter as tk
 
-def main():
+x = datetime.now()
+DateTimeSTR = '{}{}{}'.format(
+    x.year,
+    str(x.month).zfill(2) if len(str(x.month)) < 2 else str(x.month),
+    str(x.day).zfill(2) if len(str(x.day)) < 2 else str(x.day))
+
+
+def filetypesSelect(filedf, fileName, filetypesStr, check):
+    if 'csv' in filetypesStr:
+        filedf.to_csv('{}_{}.csv'.format(check, fileName), index=False, encoding='utf-8')
+    elif 'json' in filetypesStr:
+        filedf.to_json('{}_{}.json'.format(check, fileName), orient="records")
+    elif 'xlsx' in filetypesStr:
+        writer = pd.ExcelWriter('{}_{}.xlsx'.format(check, fileName), engine='xlsxwriter',
+                                options={'strings_to_urls': False})
+        filedf.to_excel(writer, index=False, encoding='utf-8')
+        writer.close()
+    elif 'msgpack' in filetypesStr:
+        filedf.to_msgpack("{}_{}.msg".format(check, fileName), encoding='utf-8')
+    elif 'feather' in filetypesStr:
+        filedf.to_feather('{}_{}.feather'.format(check, fileName))
+    elif 'parquet' in filetypesStr:
+        filedf.to_parquet('{}_{}.parquet'.format(check, fileName), engine='pyarrow', encoding='utf-8')
+    elif 'pickle' in filetypesStr:
+        filedf.to_pickle('{}_{}.pkl'.format(check, fileName))
+
+
+def change_label_number():
+    strLabel = tk.Label(window, text='處理中...')
+    strLabel.pack(anchor='center')
+    global url
+    global zipfileName
+    global comboExample
+    comboExampleget = fileTypeListbox.get(fileTypeListbox.curselection())
+    req = requests.get('https://clinicaltrials.gov/ct2/results?cond=&term=&cntry=&state=&city=&dist=')
+    soup = BeautifulSoup(req.text, 'html5lib')
+    CTDataCounts = int(''.join(list(filter(str.isdigit, soup.findAll('div', {'class': 'sr-search-terms'})[1].text))))
+    for n in tqdm(range(1, CTDataCounts // 10000 + 2 - 29), ascii=True, desc='Downloads Data -> ', ncols=69):
+        url = "https://clinicaltrials.gov/ct2/results/download_fields?down_count=10000&down_flds=all&down_fmt=csv&down_chunk={}".format(
+            n)
+        s = requests.get(url).content
+        allCTData.extend(pd.read_csv(io.StringIO(s.decode('utf-8')), encoding='utf-8').to_dict('records'))
+    allCTDataDF = pd.DataFrame(allCTData)
+    allCTDataDF = allCTDataDF.rename(
+        dict(zip(allCTDataDF.columns, [i.replace(' ', '') for i in allCTDataDF.columns])),
+        axis=1)
+    allCTDataDF_Columns = ['NCTNumber', 'Title', 'Acronym', 'Status', 'Conditions'
+        , 'Interventions', 'OutcomeMeasures', 'Sponsor/Collaborators', 'Gender', 'Age'
+        , 'Phases', 'Enrollment', 'FundedBys', 'StudyType', 'StudyDesigns'
+        , 'OtherIDs', 'StartDate', 'PrimaryCompletionDate', 'CompletionDate', 'FirstPosted'
+        , 'ResultsFirstPosted', 'LastUpdatePosted', 'Locations', 'Rank', 'StudyDocuments'
+        , 'StudyResults', 'URL']
+    # 計算三份檔案資料筆數
+    allCTDataCounts = 'Clinical Trials Count: {}'.format(len(allCTDataDF))
+    with open('ClinicalTrials_DataCounts.txt', 'w', encoding='utf-8') as txt:
+        txt.write(allCTDataCounts)
+    print('Loading Clinical Trials Data to {}'.format(comboExampleget))
+    try:
+        filetypesSelect(allCTDataDF[allCTDataDF_Columns], 'ClinicalTrials', comboExampleget, DateTimeSTR)
+        window.quit()
+    except Exception:
+        window2 = tk.Tk()
+        window2.title('錯誤提示')
+        window2.geometry('400x300')
+        error_Text = ''
+        e_type, e_value, e_traceback = sys.exc_info()
+        error_Text += f'''錯誤訊息如下：
+                        Errortype ==> {e_type.__name__}
+                        ErrorInfo ==> {e_value}
+                        ErrorFileName ==> {e_traceback.tb_frame.f_code.co_filename}
+                        ErrorLineOn ==> {e_traceback.tb_lineno}
+                        ErrorFunctionName ==> {e_traceback.tb_frame.f_code.co_name}'''
+        with open('errorFileLog.log', 'w+') as errorFileLog:
+            errorFileLog.write(error_Text)
+        strLabel2 = tk.Label(window2, text='{}\n{}\n{}'.format(e_type, e_value, e_traceback))
+        strLabel2.pack(anchor='center')
+        window2.mainloop()
+
+    finally:
+        pass
+
+
+window = tk.Tk()
+window.title('請選擇輸出檔案格式(Select File Type)')
+window.geometry('400x300')
+try:
     allCTData = []
     allCTDataDF = pd.DataFrame()
-    x = datetime.now()
-    DateTimeSTR = '{}{}{}'.format(
-        x.year,
-        str(x.month).zfill(2) if len(str(x.month)) < 2 else str(x.month),
-        str(x.day).zfill(2) if len(str(x.day)) < 2 else str(x.day))
-    try:
-        path = './{}_ClinicalTrialsData'.format(DateTimeSTR)
-        if not os.path.isdir(path):
-            os.mkdir(path)
-            os.chdir(path)
-        else:
-            os.chdir(path)
-        req = requests.get('https://clinicaltrials.gov/ct2/results?cond=&term=&cntry=&state=&city=&dist=')
-        soup = BeautifulSoup(req.text, 'html5lib')
-        CTDataCounts = int(''.join(list(filter(str.isdigit, soup.findAll('div', {'class': 'sr-search-terms'})[1].text))))
-        for n in tqdm(range(1, CTDataCounts // 10000 + 2 - 29), ascii=True, desc='Downloads Data -> ', ncols=69):
-            url = "https://clinicaltrials.gov/ct2/results/download_fields?down_count=10000&down_flds=all&down_fmt=csv&down_chunk={}".format(n)
-            s = requests.get(url).content
-            allCTData.extend(pd.read_csv(io.StringIO(s.decode('utf-8')), encoding='utf-8').to_dict('records'))
-        allCTDataDF = pd.DataFrame(allCTData)
-        allCTDataDF = allCTDataDF.rename(
-            dict(zip(allCTDataDF.columns, [i.replace(' ', '') for i in allCTDataDF.columns])),
-            axis=1)
-        allCTDataDF_Columns = ['NCTNumber', 'Title', 'Acronym', 'Status', 'Conditions'
-            , 'Interventions', 'OutcomeMeasures', 'Sponsor/Collaborators', 'Gender', 'Age'
-            , 'Phases', 'Enrollment', 'FundedBys', 'StudyType', 'StudyDesigns'
-            , 'OtherIDs', 'StartDate', 'PrimaryCompletionDate', 'CompletionDate', 'FirstPosted'
-            , 'ResultsFirstPosted', 'LastUpdatePosted', 'Locations', 'Rank', 'StudyDocuments'
-            , 'StudyResults', 'URL']
-       
-       # output excel
-        print('Loading Clinical Trials Data to Excel file.')
-        writer = pd.ExcelWriter('ClinicalTrials_{}.xlsx'.format(DateTimeSTR), engine='xlsxwriter',
-            options={'strings_to_urls': False})
-        allCTDataDF[allCTDataDF_Columns].to_excel(writer, index=False, encoding='utf-8')
-        writer.close()
-
-        # output csv
-        print('Loading Clinical Trials Data to CSV file.')
-        allCTDataDF[allCTDataDF_Columns].to_csv('ClinicalTrials_{}.csv'.format(DateTimeSTR), index=False, encoding='utf-8')
-    except Exception as e:
-        print(str(e))
-        with open('errorFileLog.log', 'w+') as errorFileLog:
-            errorFileLog.write(str(e))
-if __name__ == '__main__':
-    main()
-    print('Downloads Clinical Trials is Done.')
+    path = './{}_ClinicalTrialsData'.format(DateTimeSTR)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+        os.chdir(path)
+    else:
+        os.chdir(path)
+    fileTypeVar = tk.StringVar()
+    fileTypeVar.set(('csv', 'json', 'xlsx', 'msgpack', 'feather', 'parquet', 'pickle'))
+    fileTypeListbox = tk.Listbox(window, listvariable=fileTypeVar)
+    fileTypeListbox.pack(anchor='center')
+    saveButton = tk.Button(window, text='儲存(Save)', command=change_label_number)
+    saveButton.pack(anchor='center')
+    window.mainloop()
+except Exception as e:
+    window2 = tk.Tk()
+    window2.title('錯誤提示')
+    window2.geometry('400x300')
+    error_Text = ''
+    e_type, e_value, e_traceback = sys.exc_info()
+    error_Text += f'''錯誤訊息如下：
+                Errortype ==> {e_type.__name__}
+                ErrorInfo ==> {e_value}
+                ErrorFileName ==> {e_traceback.tb_frame.f_code.co_filename}
+                ErrorLineOn ==> {e_traceback.tb_lineno}
+                ErrorFunctionName ==> {e_traceback.tb_frame.f_code.co_name}'''
+    with open('errorFileLog.log', 'w+') as errorFileLog:
+        errorFileLog.write(error_Text)
+    strLabel2 = tk.Label(window2, text='{}\n{}\n{}'.format(e_type, e_value, e_traceback))
+    strLabel2.pack(anchor='center')
+    window2.mainloop()
+finally:
+    pass
--- README.md
+++ README.md
@@ -1,24 +1,28 @@
 # Downloads_ClinicalTrials_Data
+Downloads_ClinicalTrials_Data
 
 # 前言
 
 下載美國 [clinicaltrials.gov](https://clinicaltrials.gov/) 臨床試驗資料
 使用官方提供[Use URL Parameters to Display and Save Data](https://clinicaltrials.gov/ct2/resources/download#UseURL) 將資料下載下來後合併成單一檔案
 
-# 來源檔案更新時間
-隨時更新(一有最新資料，會直接於官網更新)
-
 # 使用套件
-
 - pandas
 - requests
 - BeautifulSoup
 - tqdm
 
-# 程式支援輸出(陸續增加中...)
+# 檔案更新時間
+隨時更新(一有最新資料，會直接於官網更新)
+
+# 支援輸出
 
 - .csv
+- .json (records)
 - .xlsx (Excel)
+- .msgpack
+- .feather
+- .parquet
 
 基於 pandas 的檔案格式
 http://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#io-tools-text-csv-hdf5
